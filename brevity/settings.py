2bc807e4ed0fd87c27480b42cd12e80d5114b066
Khatana-AZM <waqarulhaq220@gmail.com>
2024-12-29 20:53:56 +0500

    Created the Users app added a User model

--- brevity/settings.py
+++ brevity/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # custom apps
+    'apps',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
