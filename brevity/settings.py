414099129d5acfeb2536224c44a957f7a8706b36
Khatana-AZM <waqar.khatana@azm.dev>
2024-12-29 19:06:17 +0500

    database setup

--- brevity/settings.py
+++ brevity/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'brevity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'brevity',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  # or '127.0.0.1'
+        'PORT': '5432',  # default PostgreSQL port
     }
 }
+
 
 
 # Password validation
