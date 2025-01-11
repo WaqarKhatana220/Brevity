8cd77bfaa298acfef937953dd98364c83ade3f63
WaqarKhatana220 <waqarulhaq220@gmail.com>
2025-01-11 23:54:19 +0500

    testing git

--- apps/user_auth/urls.py
+++ apps/user_auth/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('register', RegisterView.as_view(), name='auth_register'),
     path('login', LoginView.as_view(), name='auth_login'),
+    sds
 ]
